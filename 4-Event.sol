a87f152651f35ec7c7a2a41beb180eb38d7a1023
Giulio Di Sano <disanogiulio@gmail.com>
2019-01-28 15:31:45 +0100

    Update 4-Event.sol

--- 4-Event.sol
+++ 4-Event.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract SimpleStorage {
     uint256 public get;
 
-    event Set(address from, uint256 amount);
+    event Set(address indexed from, uint256 amount);
 
     function set(uint x) public {
         get = x;
