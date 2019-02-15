eddead0666a2b6cf8f0535b7e37a8e79a2ef9a0b
Giulio Di Sano <disanogiulio@gmail.com>
2019-02-15 17:52:32 +0100

    Update 12-Variables.sol

--- 12-Variables.sol
+++ 12-Variables.sol
@@ -4,11 +4,13 @@ contract Variables {
     bytes32 public data;
     address public sender;
     uint256 public number;
+    string public stringa;
     mapping(address => uint256) public balancesOf;
 
     constructor() public {
         sender = msg.sender;
-        data = keccak256(abi.encodePacked("ciao"));
+        stringa = "ciao";
+        data = keccak256(abi.encodePacked(stringa));
         number = uint256(12345678);
         balancesOf[sender] = number;
     }
